f731854e4d523380b9c5d8c9df0aebfdfa598c32
rwen-ds <rong.wen@uzh.ch>
2025-04-06 08:51:05 +0200

    Create a controller for Message related to #132

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/MessageController.java
@@ -0,0 +1,38 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Message;
+import ch.uzh.ifi.hase.soprafs24.service.MessageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendToUser("/queue/messages")
+    public Message sendMessage(@Payload Message message) {
+        return messageService.sendMessage(message);
+    }
+
+    @GetMapping("/api/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<Message>> getOfflineMessages(
+            @PathVariable Long senderId,
+            @PathVariable Long recipientId) {
+        List<Message> messages = messageService.getAndMarkAsRead(senderId, recipientId);
+
+        return ResponseEntity.ok(messages);
+    }
+}

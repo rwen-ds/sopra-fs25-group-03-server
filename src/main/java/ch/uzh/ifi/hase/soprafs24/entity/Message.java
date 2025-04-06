7090c55468d5a46db8063fb6c6c2c8f1a60e2bc7
rwen-ds <rong.wen@uzh.ch>
2025-04-06 08:48:02 +0200

    create Message entity related to #129

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Message.java
@@ -0,0 +1,68 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MESSAGE")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long senderId;
+    private Long recipientId;
+    private String content;
+    private LocalDateTime timestamp;
+    private boolean isRead;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Long senderId) {
+        this.senderId = senderId;
+    }
+
+    public Long getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(Long recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+}

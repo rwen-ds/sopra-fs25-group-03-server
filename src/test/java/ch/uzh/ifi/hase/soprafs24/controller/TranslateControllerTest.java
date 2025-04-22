1070d75beedcc32cc88d9e42748ba148df6c9d79
Qinrui Deng <qinrui.deng@uzh.ch>
2025-04-22 19:21:38 +0200

    Delete some packages

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/TranslateControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/TranslateControllerTest.java
@@ -1,30 +1,21 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
-import ch.uzh.ifi.hase.soprafs24.entity.Message;
-import ch.uzh.ifi.hase.soprafs24.entity.User;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.ContactDTO;
-import ch.uzh.ifi.hase.soprafs24.security.AuthFilter;
-import ch.uzh.ifi.hase.soprafs24.service.MessageService;
-import ch.uzh.ifi.hase.soprafs24.service.TranslationService;
-import ch.uzh.ifi.hase.soprafs24.service.UserService;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import ch.uzh.ifi.hase.soprafs24.security.AuthFilter;
+import ch.uzh.ifi.hase.soprafs24.service.TranslationService;
+
 @WebMvcTest(TranslateController.class)
-@AutoConfigureMockMvc(addFilters = false)  // Disable the filter
+@AutoConfigureMockMvc(addFilters = false)
 public class TranslateControllerTest {
 
     @Autowired

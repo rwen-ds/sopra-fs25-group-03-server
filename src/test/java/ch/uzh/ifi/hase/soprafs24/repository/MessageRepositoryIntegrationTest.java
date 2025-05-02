e32e8774fc265ba361361bf86cc99715b312d605
Qinrui Deng <qinrui.deng@uzh.ch>
2025-05-02 23:18:33 +0200

    modify message integration test

--- src/test/java/ch/uzh/ifi/hase/soprafs24/repository/MessageRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/repository/MessageRepositoryIntegrationTest.java
@@ -1,27 +1,14 @@
 package ch.uzh.ifi.hase.soprafs24.repository;
 
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import ch.uzh.ifi.hase.soprafs24.constant.RequestEmergencyLevel;
-import ch.uzh.ifi.hase.soprafs24.constant.RequestStatus;
-import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
-import ch.uzh.ifi.hase.soprafs24.entity.Request;
-import ch.uzh.ifi.hase.soprafs24.entity.User;
-
-public class MessageRepositoryIntegratinTest {
+public class MessageRepositoryIntegrationTest {
         
-    // @Autowired
-    // private MessageRepository messageRepository;
+    @Autowired
+    private MessageRepository messageRepository;
 
-    // @Autowired
-    // private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     // @Test
     // public void testSaveMessage() {
